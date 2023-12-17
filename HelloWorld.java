430d1e5ab0a0f36c5ef4e2d316d71c6292643da4
LaxmiBH <lalubhutad@gmail.com>
2023-12-17 12:45:20 +0530

    Commit on Slave

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,7 @@ class HelloWorld
 	public static void main(String args[])
 	{
 	 	System.out.println("HelloWorld");
+		System.out.println("Welcome to Ditiss");
+		System.out.println("Welecome to Jungle");
 	}
 }
